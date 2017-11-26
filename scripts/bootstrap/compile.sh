4704e9d8b7626e540b7ea335290769475f26dfae
femik <femik@users.noreply.github.com>
2017-11-26 03:14:54 -0500

    Increase max JVM size for building on Edison

--- scripts/bootstrap/compile.sh
+++ scripts/bootstrap/compile.sh
@@ -113,7 +113,7 @@ function java_compilation() {
     cat "$paramfile" >&2
   fi
 
-  run "${JAVAC}" -classpath "${classpath}" -sourcepath "${sourcepath}" \
+  run "${JAVAC}" -J-Xmx512m -classpath "${classpath}" -sourcepath "${sourcepath}" \
       -d "${output}/classes" -source "$JAVA_VERSION" -target "$JAVA_VERSION" \
       -encoding UTF-8 "@${paramfile}"
 
